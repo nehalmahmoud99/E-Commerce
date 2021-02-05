59fa2f9313fc51fa762adf5af897eae9287a6211
MElmenofy <53525215+MElmenofy@users.noreply.github.com>
2021-02-05 19:55:47 +0200

    Mahmoud created repo and added project

---
+++ js/main.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+
+    var swiper = new Swiper('.swiper-container', {
+        spaceBetween: 30,
+        centeredSlides: true,
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
+
+    $('.electronics').click(function() {
+        $('#cat-container').toggle();
+    })
+
+    $('.electronics').blur(function() {
+        $('#cat-container').hide();
+    })
+});
