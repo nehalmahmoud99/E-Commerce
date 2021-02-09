05c5128f312b5c68590a28a0485e4b65819cb31b
MElmenofy <53525215+MElmenofy@users.noreply.github.com>
2021-02-09 13:32:55 +0200

    Mahmoud added Cat3

--- js/main.js
+++ js/main.js
@@ -24,4 +24,26 @@ $(document).ready(function() {
     $('.electronics').blur(function() {
         $('#cat-container').hide();
     })
+
+       // cat3 owl
+       $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 2
+            },
+            800: {
+                items: 3
+            },
+            1000: {
+                items: 4
+            }
+        }
+    })
+    /// end cat3
 });
