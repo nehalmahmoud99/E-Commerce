7c743a2a39236ed30996ad814a6448e39b52a509
Nehal Mahmoud <62676795+nehalmahmoud99@users.noreply.github.com>
2021-03-27 04:11:36 +0200

    Nehal
    edited in html ,css,js

--- js/main.js
+++ js/main.js
@@ -1,55 +1,86 @@
-$(document).ready(function() {
+(function($) {
+    "use strict";
 
-    var swiper = new Swiper('.swiper-container', {
-        spaceBetween: 30,
-        centeredSlides: true,
-        autoplay: {
-            delay: 2500,
-            disableOnInteraction: false,
-        },
-        pagination: {
-            el: '.swiper-pagination',
-            clickable: true,
-        },
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
+
+
+    $(document).ready(function() {
+
+        var swiper = new Swiper('.swiper-container', {
+            spaceBetween: 30,
+            centeredSlides: true,
+            autoplay: {
+                delay: 2500,
+                disableOnInteraction: false,
+            },
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true,
+            },
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+        });
+
+        $('.electronics').click(function() {
+            $('#cat-container').toggle();
+        })
+
+        $('.electronics').blur(function() {
+            $('#cat-container').hide();
+        })
+
+
+
+
+
+
+        // cat3 owl
+        $('.owl-carousel').owlCarousel({
+                loop: true,
+                margin: 10,
+                nav: true,
+                responsive: {
+                    0: {
+                        items: 1
+                    },
+                    600: {
+                        items: 2
+                    },
+                    800: {
+                        items: 3
+                    },
+                    1000: {
+                        items: 4
+                    }
+                }
+            })
+            /// end cat3
     });
 
-    $('.electronics').click(function() {
-        $('#cat-container').toggle();
-    })
 
-    $('.electronics').blur(function() {
-        $('#cat-container').hide();
-    })
+    const popup = document.querySelector(".popup");
+    const closePopup = document.querySelector(".popup__close");
+
+    if (popup) {
+        closePopup.addEventListener("click", () => {
+            popup.classList.add("hide__popup");
+        });
+
+        window.addEventListener("load", () => {
+            setTimeout(() => {
+                popup.classList.remove("hide__popup");
+            }, 500);
+        });
+    }
+
+
+    $(document).ready(function() {
+        'use strict';
+    });
 
 
 
 
 
-
-
-    // cat3 owl
-    $('.owl-carousel').owlCarousel({
-            loop: true,
-            margin: 10,
-            nav: true,
-            responsive: {
-                0: {
-                    items: 1
-                },
-                600: {
-                    items: 2
-                },
-                800: {
-                    items: 3
-                },
-                1000: {
-                    items: 4
-                }
-            }
-        })
-        /// end cat3
-});
+})(jQuery);
